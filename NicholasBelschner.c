a0673993d49ca88a7adab42e672182de986f8277
Nicholas Belschner <101606770+NicholasBelschner@users.noreply.github.com>
2023-08-25 10:26:42 -0400

    Update NicholasBelschner.c

--- NicholasBelschner.c
+++ NicholasBelschner.c
@@ -1,11 +1,3 @@
-/* 
-  This program takes an argument from the command line in order to execute the Collatz conjecture. 
-  Using the fork() method to create two child processes that will execute two instances of the Collatz conjecture. 
-  As a learning activity I inclued when the parent was waiting for her child process and a messeage saying they are done executing. 
-  Task 2: The child processes are not guaranteed to execute in the order it is forked because of the wait(NULL) function. 
-  The reason being is becasue it is dependent on the scheduling and execution of the processes by the operating system.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
